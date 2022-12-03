91f0defdbd40d807db1189b8940444563cac0cce
Caio Beneton <caio.beneton@gmail.com>
2022-12-03 16:45:01 -0300

    Feat: post Poll

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -2,7 +2,22 @@ import dayjs from "dayjs"
 import { pollsCollection } from "../database/db.js"
 
 export async function postPoll(req, res){
-    const now = dayjs().add(1,"M").format("YYYY-MM-DD HH:mm:ss")
+    const {title, expireAt} = req.body
+
+    try {
+        if (expireAt === ""){
+            let now = dayjs().add(1,"M").format("YYYY-MM-DD HH:mm:ss")
+            await pollsCollection.insertOne({title, expireAt: now})
+            return res.sendStatus(201)
+        }
+
+        await pollsCollection.insertOne({title, expireAt})
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500)
+        console.log(error)
+    }
+    
 }
 
 export async function getPoll(req, res){
--- src/models/poll.model.js
+++ src/models/poll.model.js
@@ -1,5 +1,6 @@
 import joi from "joi"
 
 export const pollSchema = joi.object({
-    title: joi.string().required().min(3)
+    title: joi.string().required().min(3),
+    expireAt: joi.string().allow("", null)
 })
